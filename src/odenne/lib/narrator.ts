dea7da3f5cbbe88deb882121262638274906b8fa
Alper Berber <berber@sabanciuniv.edu>
2022-03-29 04:23:32 +0300

    fixed logs

--- src/odenne/lib/narrator.ts
+++ src/odenne/lib/narrator.ts
@@ -17,16 +17,16 @@ export default class Narrator {
 
         this.events = [];
         this.templates = new Map<number, string>([
-            [EventTypes.DAMAGE, "{{ attacker }} dealt {{ damage }} with {{ skill }}"],
-            [EventTypes.REFLECT, "{{ defender }} reflected {{ damage }} with {{ skill }}"],
-            [EventTypes.DAMAGE_AND, "{{ attacker }} dealt {{ damage }} with {{ skill }} and {{ reason }}"],
-            [EventTypes.DAMAGE_CANCEL, "{{ attacker }} dealt {{ damage }} with {{ skill }} but {{ defender }} {{ reason }}"],
-            [EventTypes.CC, "{{ attacker }} was {{ skill }}"],
-            [EventTypes.STATS_INCREASE, "{{ attacker }} gained {{ statvalue }} {{ stattype }} with {{ skill }}"],
-            [EventTypes.INVULNERABLE, "{{ attacker }} became invulnerable"],
-            [EventTypes.ROUND_STEAL, "{{ attacker }} stole the next round"],
-            [EventTypes.SHIELD_GAIN, "{{ attacker }} gained {{ shieldValue }} {{ shieldType }} shield"],
-            [EventTypes.GAIN_CC_IMMUNITY, '{{ attacker }} gained immunity with {{ skill }}']
+            [EventTypes.DAMAGE, "**{{ attacker }}** dealt `{{ damage }}` with **{{ skill }}**"],
+            [EventTypes.REFLECT, "**{{ defender }}** reflected `{{ damage }}` with **{{ skill }}**"],
+            [EventTypes.DAMAGE_AND, "**{{ attacker }}** dealt `{{ damage }}` with **{{ skill }}** and {{ reason }}"],
+            [EventTypes.DAMAGE_CANCEL, "**{{ attacker }}** dealt `{{ damage }}` with **{{ skill }}** but **{{ defender }}** {{ reason }}"],
+            [EventTypes.CC, "**{{ attacker }}** was **{{ skill }}**"],
+            [EventTypes.STATS_INCREASE, "**{{ attacker }}** gained `{{ statvalue }}` {{ stattype }} with **{{ skill }}**"],
+            [EventTypes.INVULNERABLE, "**{{ attacker }}** became invulnerable"],
+            [EventTypes.ROUND_STEAL, "**{{ attacker }}** stole the next round"],
+            [EventTypes.SHIELD_GAIN, "**{{ attacker }}** gained `{{ shieldValue }}` {{ shieldType }} shield"],
+            [EventTypes.GAIN_CC_IMMUNITY, '**{{ attacker }}** gained immunity with **{{ skill }}**']
         ]);
 
         this.priority = [
