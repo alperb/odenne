28aecaf894aa29fe481ee36c07484d27c3370708
Alper Berber <berber@sabanciuniv.edu>
2022-05-06 03:54:45 +0300

    add: odenne startup script

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
-yarn install
-yarn build
-node ./dist/server.js
+value=$(<odenne.version)
+docker build -t odenne:$value .
+
+docker run -d -p 3101:4999 --name odenne_worker_1 odenne:$value
+docker run -d -p 3102:4999 --name odenne_worker_2 odenne:$value
