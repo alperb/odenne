3390961f47543644c93b6f9868ad734ed8a63672
Edip Emre Bodur <emrebdr29@gmail.com>
2022-06-02 03:37:08 +0300

    start.sh

--- start.sh
+++ start.sh
@@ -3,5 +3,6 @@ echo "Starting 2 instances of odenne:$value"
 
 docker run -d -p 3101:4999 --name odenne_worker_1 odenne:$value
 docker run -d -p 3102:4999 --name odenne_worker_2 odenne:$value
+docker run -d -p 3103:4999 --name odenne_worker_3 odenne:$value
 
 echo "Started odenne:$value in $SECONDS seconds"
