73ce92551b82ebdd166855f6b41dddec4a2ab283
root <root@vmi912775.contaboserver.net>
2022-08-05 10:02:45 +0200

    ss

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 value=$(<odenne.version)
 echo "Starting 2 instances of odenne:$value"
 
-docker run -d -p 3101:4999 --name odenne_worker_1 odenne:$value
+docker run -d -p 3101:4999 -p 5021:5020 --name odenne_worker_1 odenne:$value
 docker run -d -p 3102:4999 --name odenne_worker_2 odenne:$value
 docker run -d -p 3103:4999 --name odenne_worker_3 odenne:$value
 
